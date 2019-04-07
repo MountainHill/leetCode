a80ad075979d503d118ae959566bb8a46c63584e
dingyl <17050417@suning-sports.com>
2019-04-07 11:49:06 +0800

    初始化工程文件

---
+++ leetCode/leetCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  leetCode
+//
+//  Created by dingyl on 2019/4/7.
+//  Copyright © 2019年 mountainhill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ leetCode/leetCode/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  leetCode
+//
+//  Created by dingyl on 2019/4/7.
+//  Copyright © 2019年 mountainhill. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
